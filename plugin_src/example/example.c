32afde1bd5afe998c844cf6d16795c2ac841f774
Jan200101 <sentrycraft123@gmail.com>
2020-07-10 18:34:55 +0200

    add example plugin

---
+++ plugin_src/example/example.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include "pc/plugins.h"
+
+void loop_func()
+{
+	//printf("I get called every frame\n");
+}
+
+void init_func()
+{
+	//printf("I get called when loaded\n");
+
+	add_function(loop_func);
+}
+
+void deinit_func()
+{
+	//printf("I get called when unloaded\n");
+}
